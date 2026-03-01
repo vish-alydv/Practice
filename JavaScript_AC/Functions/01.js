a0e809db6b0526de725b0e31c5a05aaa2b1897f5
vish-alydv <thevishaleditor@gmail.com>
2026-03-01 20:51:25 +0530

    Practice

---
+++ JavaScript_AC/Functions/01.js
@@ -0,0 +1,19 @@
+// function myfunction(){
+//     console.log("Printing..");
+// }
+
+// myfunction()
+
+
+// function myfunction(p){
+//     console.log(p);
+// }
+
+// myfunction("Hi")
+
+
+const Sum = (a,b)=>{
+    console.log(a+b);
+}
+
+Sum(2,3)
