ae7f7c1b46504b1ccf13078afa6d1c74171a96b7
vish-alydv <thevishaleditor@gmail.com>
2026-03-07 14:32:59 +0530

    Practice

---
+++ JavaScript_AC/DOM_Part_1/script.js
@@ -0,0 +1,16 @@
+// let heading = document.getElementsByClassName("heading class")
+
+// console.log(heading)
+
+// // let heading = document.getElementById("heading")
+
+// // console.log(heading)
+
+
+
+let firstElement = document.querySelector("p");
+console.dir(firstElement)
+
+
+let AllElement = document.querySelectorAll("p");
+console.dir(AllElement)
