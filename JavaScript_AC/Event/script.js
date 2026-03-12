8904c54644829cc1521343a863449b21b735f707
vish-alydv <thevishaleditor@gmail.com>
2026-03-12 22:14:40 +0530

    Java Script

---
+++ JavaScript_AC/Event/script.js
@@ -0,0 +1,11 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = ()=> {
+    console.log("Button Was Clicked");
+}
+
+
+let div = document.querySelector("div");
+div.onmouseover = () => {
+    console.log("You Are Inside Div");
+}
