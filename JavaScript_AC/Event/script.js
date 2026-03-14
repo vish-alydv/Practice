f1e8caca9465930d1a0e98f2f06aab51332f1132
vish-alydv <thevishaleditor@gmail.com>
2026-03-14 19:33:23 +0530

    Back Again

--- JavaScript_AC/Event/script.js
+++ JavaScript_AC/Event/script.js
@@ -1,11 +1,37 @@
 let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = ()=> {
-    console.log("Button Was Clicked");
-}
+// btn1.onclick = ()=> {
+//     console.log("Button Was Clicked");
+// }
 
 
-let div = document.querySelector("div");
-div.onmouseover = () => {
-    console.log("You Are Inside Div");
+// let div = document.querySelector("div");
+// div.onmouseover = () => {
+//     console.log("You Are Inside Div");
+// }
+
+
+btn1.addEventListener("click",(evt)=>{
+    console.log("Button 1 Was Clicked");
+    // console.log(evt);
+    // console.log(evt.type);
+});
+
+
+
+btn1.addEventListener("click",()=>{
+    console.log("Button 1 Was Clicked 2nd Time");
+});
+
+
+let function3 = () =>{
+    console.log("Button 1 Was Clicked 3rd Time");
 }
+
+btn1.addEventListener("click",function3);
+btn1.addEventListener("click",()=>{
+    console.log("Button 1 Was Clicked 4th Time");
+});
+
+
+btn1.removeEventListener("click",function3);
