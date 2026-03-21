61c08336688b4315973bae85102ef6fd0c54a87e
vish-alydv <thevishaleditor@gmail.com>
2026-03-21 17:50:56 +0530

    Practice

---
+++ JavaScript_AC/CallBack/script.js
@@ -0,0 +1,78 @@
+const getPromsie = ()=> {
+    return new promise((resolve,reject)=>{
+        console.log("I am Promise");
+        resolve("Succes");
+    });
+};
+
+
+let promise = getPromsie();
+promise.then((res)=>{
+    console.log("Promise Fullfilled",res);
+});
+
+promise.catch((err)=>{
+    console.log("Rejected",err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getData(DataId,getNextData){
+//     setTimeout(()=>{
+//         console.log("Data",DataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     },2000);
+// }
+
+
+
+// getData(1,()=>{
+//     console.log("Getting Data @...");
+//     getData(2,()=>{
+//         console.log("Getting Data @...");
+//         getData(3,()=>{
+//             console.log("Getting Data @...");
+//             getData(4);
+//         });
+//     });
+// });
