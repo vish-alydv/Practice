5f39e2d0f4bbab676e9cdd1daacf1499054d9729
vish-alydv <thevishaleditor@gmail.com>
2026-06-03 11:36:35 +0530

    Gsap

--- Gsap/03/script.js
+++ Gsap/03/script.js
@@ -1,6 +1,62 @@
-gsap.from("#page1 #box",{
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:2,
+//     duration:2,
+//     rotate:360,
+// })
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+
+
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scorller:"body",
+//         markers:true,
+//         start:"top 50%",
+//     }
+// })
+
+// gsap.from("#page2 h2",{
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scorller:"body",
+//         markers:true,
+//         start:"top 50%",
+//     }
+// })
+
+
+
+gsap.from("#page2 #box",{
     scale:0,
-    delay:2,
-    duration:2,
-    rotate:360,
+    opacity:0,
+    duration:1,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 30%",
+        scrub:2,
+        pin:true
+    }
 })
