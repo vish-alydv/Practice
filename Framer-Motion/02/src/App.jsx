3e93cc8ac49a363be992788a25f2fb7f61edf5d5
vish-alydv <thevishaleditor@gmail.com>
2026-06-05 15:13:16 +0530

    Motion

---
+++ Framer-Motion/02/src/App.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { motion } from "motion/react"
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <motion.div
+//         // initial={{
+//         //   x: 0,
+//         //   y: 0
+//         // }}
+//         // animate={{
+//         //   x: [0,800,800,0,0],
+//         //   y: [0,0,300,300,0],
+//         //   rotate:[0,360,0,-360,0]
+//         // }}
+//         // transition={{
+//         //   duration: 5,
+//         //   delay:1
+//         // }}
+//         // whileHover={{
+//         //   backgroundColor:"green"
+//         // }}
+//         // whileTap={{
+//         //   scale:2,
+//         //   x:200
+//         // }}
+//         drag
+//         whileDrag={{
+//           scale:0.2
+//         }}
+//         dragConstraints={{
+//           left:0,
+//           right:0,
+//           right:1000,
+//           bottom:500
+//         }}
+//         className='box'></motion.div>
+//     </div>
+//   )
+// }
+
+// export default App
